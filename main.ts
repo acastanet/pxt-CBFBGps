a832408908fffa9944451c0ee86d725d7a18a0f3
sntlpdn <52889215+sntlpdn@users.noreply.github.com>
2019-07-15 16:52:54 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -14,13 +14,13 @@ namespace CBFBGps {
     //% blockId="readGPS" block="readGPS rx %rx|tx %tx|vitesse %vitesse"
     export function readGPS(rx: SerialPin, tx: SerialPin, vitesse: number): string {
         
-     /*PinName txn;
+     PinName txn;
       PinName rxn;
       if (serial.tryResolvePin(tx, txn) && serial.tryResolvePin(rx, rxn))
 	  {
         uBit.serial.redirect(txn, rxn);
       uBit.serial.baud((int)vitesse);}
-      int n = uBit.serial.getRxBufferSize();
+     /* int n = uBit.serial.getRxBufferSize();
      if (n > 0) return PSTR(uBit.serial.read(1, MicroBitSerialMode::ASYNC))*/
 	  return "coucou";
         
